f8d1906b5f380021a7ccd9e12e6ced48ae349d78
maxmillionbeatz <maxmillionbeatz@gmail.com>
2023-01-30 11:01:42 +0100

    .

--- bin.js
+++ bin.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 import { spawn } from 'child_process';
+const [, , ...args] = process.argv;
 
 const repositoryUrl = 'https://github.com/maxmillionbeatz/sveltekit-tailwind-starter.git';
-const destination = process.cwd();
+let destination = process.cwd();
+
+if (args[0]) {
+	destination = `${destination}/${args[0]}`;
+}
 
 spawn('git', ['clone', repositoryUrl, destination], { stdio: 'inherit' }).on('close', (code) => {
 	console.log(`Cloned your repo at ${destination}`);
